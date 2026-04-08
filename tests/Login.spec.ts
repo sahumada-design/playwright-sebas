6319bc54a3b1a37493372a426fd1baa3ce3d0cd5
Lucas Gonzalez <lgmartinez@huenei.com>
2026-04-08 15:57:52 -0300

    sebas

---
+++ tests/Login.spec.ts
@@ -0,0 +1,10 @@
+import { test, expect } from '@playwright/test';
+
+test('sebastest', async ({ page }) => {
+  await page.goto('https://www.google.com/?hl=es');
+  await page.waitForTimeout(9000)
+  await page.locator('//*[@id="APjFqb"]').fill('diario el sol')
+  await page.keyboard.press('Enter')
+  await page.waitForTimeout(9000)
+  
+});
---
+++ tests/sebaTESTCASE.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+
+//test('sebastest', async ({ page }) => {
+//    
+//  await page.goto('https://www.google.com/?hl=es');
+//  await page.waitForTimeout(9000)
+//  await page.locator('//*[@id="APjFqb"]').fill('diario el sol')
+//  await page.keyboard.press('Enter')
+//  console.log('presiono enter')
+//  await page.waitForTimeout(9000)
+//  
+//});
+
+test('mi test', async ({ page }) => {
+  console.log('Arrancó el test');
+
+  await page.goto('https://google.c3om');
+
+  console.log('Entré a Google');
+});

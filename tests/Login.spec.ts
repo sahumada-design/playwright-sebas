dbac4e29e39e46cb602aa2d51d4f58efe5ce3974
Lucas Gonzalez <lgmartinez@huenei.com>
2026-04-13 14:49:38 -0300

    sebasTEST

--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('sebastest', async ({ page }) => {
   await page.goto('https://www.google.com/?hl=es');
-  await page.waitForTimeout(9000)
+  await page.waitForTimeout(3000)
   await page.locator('//*[@id="APjFqb"]').fill('diario el sol')
   await page.keyboard.press('Enter')
   await page.waitForTimeout(9000)
